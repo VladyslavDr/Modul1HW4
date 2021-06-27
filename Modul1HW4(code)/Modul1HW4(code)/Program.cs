c3c5f0479374979d1083d4eacdef65bc4bcb5bc9
WIN-T2O982ERQRV\User <vladislav.drushliak@gmail.com>
2021-06-27 20:26:20 +0300

    Вывод на экран количество букв в верхнем регистре
    - Посчитал количество букв в верхнем регистре в массивах. Сравнил где их больше.
    - Сделал красивый вывод.

--- Modul1HW4(code)/Modul1HW4(code)/Program.cs
+++ Modul1HW4(code)/Modul1HW4(code)/Program.cs
@@ -4,10 +4,10 @@
 Создать 2 массива, где в 1 массиве будут значение только четных значений, а во втором нечетных.+
 Заменить числа в 1 и 2 массиве на буквы английского алфавита.+
 Значения ячеек этих массивов равны порядковому номеру каждой буквыв алфавите.+
-Если же буква является одной из списка (a, e, i, d, h, j) то она должна быть в верхнем регистре.
-Вывести на экран результат того, в каком из массивов будет больше букв в верхнем регистре.
-Вывести оба массива на экран.
-Каждый из массивов должен быть выведен 1 строкой, где его значения будут разделены пробелом.
+Если же буква является одной из списка (a, e, i, d, h, j) то она должна быть в верхнем регистре.+
+Вывести на экран результат того, в каком из массивов будет больше букв в верхнем регистре.+
+Вывести оба массива на экран.+
+Каждый из массивов должен быть выведен 1 строкой, где его значения будут разделены пробелом.+
  */
 
 using System;
@@ -43,23 +43,65 @@ foreach (var i in arrayOfIntegers)
 }
 
 // для наглядности
-Console.WriteLine(string.Join(" ", arrayOfIntegers));
-Console.WriteLine(string.Join(" ", arrayOfEvenIntegers));
-Console.WriteLine(string.Join(" ", arrayOfOddIntegers));
+Console.WriteLine();
+Console.WriteLine($"arrayOfIntegers[]: {string.Join(" ", arrayOfIntegers)}");
+Console.WriteLine();
+Console.WriteLine($"arrayOfEvenIntegers[]: {string.Join(" ", arrayOfEvenIntegers)}");
+Console.WriteLine($"arrayOfOddIntegers[]: {string.Join(" ", arrayOfOddIntegers)}");
+Console.WriteLine();
 
-var arrayOfEvenLetter = ReplaceNumbersWithLetters(arrayOfEvenIntegers);
-var arrayOfOddLetter = ReplaceNumbersWithLetters(arrayOfOddIntegers);
-
-// для наглядности
-Console.WriteLine(string.Join(" ", arrayOfEvenLetter));
-Console.WriteLine(string.Join(" ", arrayOfOddLetter));
-
-SubstitutionOfLettersByConditionToUppercase(arrayOfEvenLetter);
-SubstitutionOfLettersByConditionToUppercase(arrayOfOddLetter);
+var arrayOfEvenLetters = ReplaceNumbersWithLetters(arrayOfEvenIntegers);
+var arrayOfOddLetters = ReplaceNumbersWithLetters(arrayOfOddIntegers);
 
 // для наглядности
-Console.WriteLine(string.Join(" ", arrayOfEvenLetter));
-Console.WriteLine(string.Join(" ", arrayOfOddLetter));
+Console.WriteLine($"arrayOfEvenLetters[]: {string.Join(" ", arrayOfEvenLetters)}");
+Console.WriteLine($"arrayOfOddLetters[]: {string.Join(" ", arrayOfOddLetters)}");
+Console.WriteLine();
+
+SubstitutionOfLettersByConditionToUppercase(arrayOfEvenLetters);
+SubstitutionOfLettersByConditionToUppercase(arrayOfOddLetters);
+
+// для наглядности
+Console.WriteLine($"arrayOfEvenLetters[]: {string.Join(" ", arrayOfEvenLetters)}");
+Console.WriteLine($"arrayOfOddLetters[]: {string.Join(" ", arrayOfOddLetters)}");
+Console.WriteLine();
+
+var quantityEvenLetters = NumberOfUppercaseLettersInArray(arrayOfEvenLetters);
+var quantityOddLetters = NumberOfUppercaseLettersInArray(arrayOfOddLetters);
+
+Console.WriteLine($"quantityEvenLetters: {quantityEvenLetters}");
+Console.WriteLine($"quantityOddLetters: {quantityOddLetters}");
+Console.WriteLine();
+
+if (quantityEvenLetters > quantityOddLetters)
+{
+    Console.WriteLine("The number of capital letters is greater in the array: arrayOfEvenLetters[]");
+}
+else if (quantityEvenLetters < quantityOddLetters)
+{
+    Console.WriteLine("The number of capital letters is greater in the array: arrayOfOddLetters[]");
+}
+else
+{
+    Console.WriteLine("The number of capital letters is equally");
+}
+
+int NumberOfUppercaseLettersInArray(string[] arrayOfLetters)
+{
+    var counterUppercaseLetter = 0;
+
+    foreach (var letter in arrayOfLetters)
+    {
+        var temp = letter;
+
+        if (temp.ToUpper() == letter)
+        {
+            counterUppercaseLetter++;
+        }
+    }
+
+    return counterUppercaseLetter;
+}
 
 void SubstitutionOfLettersByConditionToUppercase(string[] arrayOfLetter)
 {
