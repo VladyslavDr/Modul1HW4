ac1da79680d840d72c4af40178467f9c36357108
WIN-T2O982ERQRV\User <vladislav.drushliak@gmail.com>
2021-06-27 19:12:45 +0300

    Разбить массив на чётные и нечётные элементы
    - Определил количество четных и нечётных чисел в массиве.
    - Создал массивы чётных и нечётных елементов.

--- Modul1HW4(code)/Modul1HW4(code)/Program.cs
+++ Modul1HW4(code)/Modul1HW4(code)/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+Создать массив на N элементов, где N указывается с консольной строки.+
+Заполнить его случайными числами от 1 до 26 включительно.+
+Создать 2 массива, где в 1 массиве будут значение только четных значений, а во втором нечетных.+
+Заменить числа в 1 и 2 массиве на буквы английского алфавита.
+Значения ячеек этих массивов равны порядковому номеру каждой буквыв алфавите.
+Если же буква является одной из списка (a, e, i, d, h, j) то она должна быть в верхнем регистре.
+Вывести на экран результат того, в каком из массивов будет больше букв в верхнем регистре.
+Вывести оба массива на экран.
+Каждый из массивов должен быть выведен 1 строкой, где его значения будут разделены пробелом.
+ */
+
+using System;
 
 Console.Write("Enter array size: ");
 
@@ -8,6 +20,58 @@ var arrayOfIntegers = new int[arraySize];
 
 FillingArrayWithRandomNumbers(arrayOfIntegers, 1, 27);
 
+var arraySizeEven = GetNumberOfEvenElementsInTheArray(arrayOfIntegers);
+var arraySizeOdd = GetNumberOfOddElementsInTheArray(arrayOfIntegers);
+
+var arrayOfEvenIntegers = new int[arraySizeEven];
+var arrayOfOddIntegers = new int[arraySizeOdd];
+
+var counterForEven = 0;
+var counterForOdd = 0;
+
+// заполнение чётных и нечётных массивов
+foreach (var i in arrayOfIntegers)
+{
+    if (i % 2 == 0)
+    {
+        arrayOfEvenIntegers[counterForEven++] = i;
+    }
+    else
+    {
+        arrayOfOddIntegers[counterForOdd++] = i;
+    }
+}
+
+int GetNumberOfEvenElementsInTheArray(int[] arrayOfIntegers)
+{
+    var counterEven = 0;
+
+    foreach (var i in arrayOfIntegers)
+    {
+        if (i % 2 == 0)
+        {
+            counterEven++;
+        }
+    }
+
+    return counterEven;
+}
+
+int GetNumberOfOddElementsInTheArray(int[] arrayOfIntegers)
+{
+    var counterOdd = 0;
+
+    foreach (var i in arrayOfIntegers)
+    {
+        if (i % 2 != 0)
+        {
+            counterOdd++;
+        }
+    }
+
+    return counterOdd;
+}
+
 void FillingArrayWithRandomNumbers(int[] arrayOfIntegers, int min, int max)
 {
     for (var i = 0; i < arrayOfIntegers.Length; i++)
@@ -15,3 +79,7 @@ void FillingArrayWithRandomNumbers(int[] arrayOfIntegers, int min, int max)
         arrayOfIntegers[i] = new Random().Next(min, max);
     }
 }
+
+Console.WriteLine(string.Join(" ", arrayOfIntegers));
+Console.WriteLine(string.Join(" ", arrayOfEvenIntegers));
+Console.WriteLine(string.Join(" ", arrayOfOddIntegers));
