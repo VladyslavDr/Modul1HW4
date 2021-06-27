466743e27f77a5422057d13329285a9d254659bf
WIN-T2O982ERQRV\User <vladislav.drushliak@gmail.com>
2021-06-27 19:33:49 +0300

    Заменить числа в массиве на буквы английского алфавита
    - Добавил enum Alphabet
    - Создал массивы с буквами англ алфавита

--- Modul1HW4(code)/Modul1HW4(code)/Program.cs
+++ Modul1HW4(code)/Modul1HW4(code)/Program.cs
@@ -2,7 +2,7 @@
 Создать массив на N элементов, где N указывается с консольной строки.+
 Заполнить его случайными числами от 1 до 26 включительно.+
 Создать 2 массива, где в 1 массиве будут значение только четных значений, а во втором нечетных.+
-Заменить числа в 1 и 2 массиве на буквы английского алфавита.
+Заменить числа в 1 и 2 массиве на буквы английского алфавита.+
 Значения ячеек этих массивов равны порядковому номеру каждой буквыв алфавите.
 Если же буква является одной из списка (a, e, i, d, h, j) то она должна быть в верхнем регистре.
 Вывести на экран результат того, в каком из массивов будет больше букв в верхнем регистре.
@@ -42,6 +42,30 @@ foreach (var i in arrayOfIntegers)
     }
 }
 
+// для наглядности
+Console.WriteLine(string.Join(" ", arrayOfIntegers));
+Console.WriteLine(string.Join(" ", arrayOfEvenIntegers));
+Console.WriteLine(string.Join(" ", arrayOfOddIntegers));
+
+var arrayOfEvenLetter = ReplaceNumbersWithLetters(arrayOfEvenIntegers);
+var arrayOfOddLetter = ReplaceNumbersWithLetters(arrayOfOddIntegers);
+
+// для наглядности
+Console.WriteLine(string.Join(" ", arrayOfEvenLetter));
+Console.WriteLine(string.Join(" ", arrayOfOddLetter));
+
+string[] ReplaceNumbersWithLetters(int[] arrayOfIntegers)
+{
+    var arrayOfLetter = new string[arrayOfIntegers.Length];
+
+    for (var i = 0; i < arrayOfIntegers.Length; i++)
+    {
+        arrayOfLetter[i] = Enum.GetName(typeof(Alphabet), arrayOfIntegers[i]).ToLower();
+    }
+
+    return arrayOfLetter;
+}
+
 int GetNumberOfEvenElementsInTheArray(int[] arrayOfIntegers)
 {
     var counterEven = 0;
@@ -79,7 +103,3 @@ void FillingArrayWithRandomNumbers(int[] arrayOfIntegers, int min, int max)
         arrayOfIntegers[i] = new Random().Next(min, max);
     }
 }
-
-Console.WriteLine(string.Join(" ", arrayOfIntegers));
-Console.WriteLine(string.Join(" ", arrayOfEvenIntegers));
-Console.WriteLine(string.Join(" ", arrayOfOddIntegers));
