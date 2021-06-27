7a83e957f4599d8ef04b8460290a3be2e70f0183
WIN-T2O982ERQRV\User <vladislav.drushliak@gmail.com>
2021-06-27 17:24:32 +0300

    Добавил gitignore и styleCop

---
+++ Modul1HW4(code)/Modul1HW4(code)/alphabet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public enum Alphabet
+{
+    A = 1,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O,
+    P,
+    Q,
+    R,
+    S,
+    T,
+    U,
+    V,
+    W,
+    X,
+    Y,
+    Z
+}
