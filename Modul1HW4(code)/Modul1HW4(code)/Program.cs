b4c087cdb3254eba9451ab534af9a3e7df1a43c1
WIN-T2O982ERQRV\User <vladislav.drushliak@gmail.com>
2021-06-27 18:32:45 +0300

    Создать массив случайных чисел
    - Считал размер массива с консоли.
    - Создал массив рандомных чисел.

--- Modul1HW4(code)/Modul1HW4(code)/Program.cs
+++ Modul1HW4(code)/Modul1HW4(code)/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+Console.Write("Enter array size: ");
+
+int.TryParse(Console.ReadLine(), out var arraySize);
+
+var arrayOfIntegers = new int[arraySize];
+
+FillingArrayWithRandomNumbers(arrayOfIntegers, 1, 27);
+
+void FillingArrayWithRandomNumbers(int[] arrayOfIntegers, int min, int max)
+{
+    for (var i = 0; i < arrayOfIntegers.Length; i++)
+    {
+        arrayOfIntegers[i] = new Random().Next(min, max);
+    }
+}
